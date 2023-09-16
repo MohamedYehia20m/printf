5c2ade176ab239b32879855f01ec10186e5e4544
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-16 16:12:30 -0700

    add function to print binary

---
+++ _printf_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _printf_binary - print binary equivalent of the
+ *					passed integer on the terminal.
+ * @ap: argument pointer.
+ *
+ * Return: length of the binary printed.
+ */
+int _printf_binary(va_list ap)
+{
+	char *str = convert_to_binary(va_arg(ap, int));
+
+	_puts(str);
+	return (_strlen(str));
+}
