1f295479d5baeaebc67a62869383a62cb9b9d665
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-16 17:24:40 -0700

    add function to convert integer to hexadecimal in lowercase letters

---
+++ convert_to_hexadecimal_lowercase.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * convert_to_hexadecimal_lowercase - convert integer
+ *		to equivalent hexadecimal (in lowercase letters).
+ * @n: integer to be converted.
+ *
+ * Return: pointer to string.
+ */
+char *convert_to_hexadecimal_lowercase(int n)
+{
+	int len = hexLenOf(n), ctr = 0, tmp;
+	char *str;
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	while (ctr <= len)
+	{
+		tmp = n % 16;
+		if (tmp < 10)
+			str[ctr] = tmp + '0';
+		else
+			str[ctr] = tmp + 'a' - 10;
+		n /= 16;
+		ctr++;
+	}
+	str[ctr] = '\0';
+	rev_string(str);
+	return (str);
+}
