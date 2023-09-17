96c961a6408fa434c5c02a1435891d11503e0163
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-16 17:27:07 -0700

    add function to print hexadecimal in lowercase letters

---
+++ _printf_hexadecimal_lowercase.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _printf_hexadecimal_lowercase - print hexadecimal
+ *		equivalent of the passed integer on the terminal.
+ * @ap: argument pointer.
+ *
+ * Return: length of the hexadecimal printed.
+ */
+int _printf_hexadecimal_lowercase(va_list ap)
+{
+	char *str = convert_to_hexadecimal_lowercase(va_arg(ap, int));
+
+	_puts(str);
+	return (_strlen(str));
+}
