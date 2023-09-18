c616b5c9e4811664929601ebd13907c597544c6d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-18 07:26:45 -0700

    change single qoutes by double qoutes

--- get_spec.c
+++ get_spec.c
@@ -11,18 +11,18 @@
 int (*get_spec(char *s))(va_list ap)
 {
 	spec_t specs[] = {
-		{'c', _printf_character},
-		{'s', _printf_string_lowercase},
-		{'%', _printf_percentage},
-		{'d', _printf_decimal},
-		{'i', _printf_integer},
-		{'b', _printf_binary},
-		{'u', _printf_unsigned_integer},
-		{'o', _printf_octal},
-		{'x', _printf_hexadecimal_lowercase},
-		{'X', _printf_hexadecimal_uppercase},
-		{'S', _printf_string_uppercase},
-		{'p', _printf_address},
+		{"c", _printf_character},
+		{"s", _printf_string_lowercase},
+		{"%", _printf_percentage},
+		{"d", _printf_decimal},
+		{"i", _printf_integer},
+		{"b", _printf_binary},
+		{"u", _printf_unsigned_integer},
+		{"o", _printf_octal},
+		{"x", _printf_hexadecimal_lowercase},
+		{"X", _printf_hexadecimal_uppercase},
+		{"S", _printf_string_uppercase},
+		{"p", _printf_address},
 		{NULL, NULL}
 	};
 	int i = 0;
