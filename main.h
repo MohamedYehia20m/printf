48e41eb2f48d974b357c8b1fce9143a4367fe151
MohamedYehia20m <medoyehia2001@gmail.com>
2023-09-18 21:40:11 +0300

    adding _printf_percentage

--- main.h
+++ main.h
@@ -63,12 +63,13 @@ int hexLenOf(int n);
 int _printf_hexadecimal_uppercase(va_list ap);
 char *convert_to_hexadecimal_uppercase(int n);
 int _printf_decimal(va_list ap);
-unsigned int _printf_unsigned_integer(va_list ap);
+int _printf_unsigned_integer(va_list ap);
 int TwosComplement(int c);
 int _printf_octal(va_list ap);
 int convert_to_octal(int argument);
 int _printf_address(va_list ap);
 int _printf_string_uppercase(va_list ap);
 int _putchar_nonPrintable(char c);
+int _printf_percentage(va_list ap);
 
 #endif
