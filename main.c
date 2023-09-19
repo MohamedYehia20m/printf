acb98b9ced7b315f68bf997b7a1220ec60587d8a
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-19 09:06:20 -0700

    add test cases

--- main.c
+++ main.c
@@ -13,13 +13,18 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
+    char *strtest = "ABCD";
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
+    _printf("Negative Length:[%d, %i]\n", -1 * len, -1 * len);
+    printf("Negative Length:[%d, %i]\n", -1 * len2, -1 * len2);
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
+    _printf("NULL Length:[%d, %i]\n", NULL, NULL);
+    printf("NULL Length:[%d, %i]\n", NULL, NULL);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
@@ -48,8 +53,12 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    /*_printf("Unknown:[%r]\n");*/
-    /*printf("Unknown:[%r]\n");*/
+    _printf("revert:[%r]\n", strtest);
+    printf("revert:[%r]\n", strtest);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    _printf("rot13:[%R]\n", strtest);
+    printf("rot13:[%R]\n", strtest);
     _printf("binary: %b\n", 98);
     /*printf("binary: %b\n", 98);*/
     _printf("%S\n", "Best\nSchool");
